0c84df412fe464cf121d284f0871b31f07848ff5
CupOfOwls <thoemmes.stephen@gmail.com>
2025-05-25 14:23:00 -0600

    Initial commit

---
+++ test_all_examples.sh
@@ -0,0 +1,14 @@
+# /usr/bin/bash
+
+# This script runs all the examples in the examples directory.
+# It assumes that the examples are in a directory called "examples" in the current directory.
+
+source venv/bin/activate
+
+python examples/cart_api_examples.py
+python examples/identity_api_examples.py
+python examples/location_api_examples.py
+python examples/product_api_examples.py
+python examples/authorization_api_examples.py
+python examples/oauth_flow.py
+python examples/token_refresh_example.py
